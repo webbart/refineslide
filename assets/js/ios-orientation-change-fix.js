0a9424157631e98b285809e59de0670ab6b4c3e0
Alex Dunphy <alexanderdunphy@gmail.com>
2012-03-01 15:59:15 +0000

    v0.3 bump - minor JS enhancements & CSS alterations
    - Updated packaged imagesLoaded plugin to v2.0.1 (https://github.com/desandro/imagesloaded)
    
    - Condensed jQuery .on()/.off() calls to .one()
    
    - Superficial CSS changes (non-layout)
    
    - Added .gitignore with a few stock files to ignore

--- assets/js/ios-orientation-change-fix.js
+++ assets/js/ios-orientation-change-fix.js
@@ -1,13 +1,13 @@
 /*! A fix for the iOS orientationchange zoom bug.
  Script by @scottjehl, rebound by @wilto.
  MIT License.
-*/
+ */
 (function(w){
 
-	// This fix addresses an iOS bug, so return early if the UA claims it's something else.
-	if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
-		return;
-	}
+    // This fix addresses an iOS bug, so return early if the UA claims it's something else.
+    if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
+        return;
+    }
 
     var doc = w.document;
 
@@ -18,7 +18,7 @@
         disabledZoom = initialContent + ",maximum-scale=1",
         enabledZoom = initialContent + ",maximum-scale=10",
         enabled = true,
-		x, y, z, aig;
+        x, y, z, aig;
 
     if( !meta ){ return; }
 
@@ -33,23 +33,23 @@
     }
 
     function checkTilt( e ){
-		aig = e.accelerationIncludingGravity;
-		x = Math.abs( aig.x );
-		y = Math.abs( aig.y );
-		z = Math.abs( aig.z );
+        aig = e.accelerationIncludingGravity;
+        x = Math.abs( aig.x );
+        y = Math.abs( aig.y );
+        z = Math.abs( aig.z );
 
-		// If portrait orientation and in one of the danger zones
+        // If portrait orientation and in one of the danger zones
         if( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
-			if( enabled ){
-				disableZoom();
-			}
+            if( enabled ){
+                disableZoom();
+            }
         }
-		else if( !enabled ){
-			restoreZoom();
+        else if( !enabled ){
+            restoreZoom();
         }
     }
 
-	w.addEventListener( "orientationchange", restoreZoom, false );
-	w.addEventListener( "devicemotion", checkTilt, false );
+    w.addEventListener( "orientationchange", restoreZoom, false );
+    w.addEventListener( "devicemotion", checkTilt, false );
 
 })( this );
